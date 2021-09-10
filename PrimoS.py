35c97582ba7ede55c3595a0e5f01a46362049954
Andrés Pimienta <52270065+arpimienta@users.noreply.github.com>
2021-09-09 23:17:46 -0500

    Add files via upload

---
+++ PrimoS.py
@@ -0,0 +1,20 @@
+def primo():
+    numero = int(input("ingresa un numero "))
+    valor= range(2,numero)
+    contador = 0
+    for n in valor:
+        if numero % n == 0:
+            contador +=1
+            print("divisor:", n)
+ 
+    if contador > 0 :
+        print("El número no es primo" )
+    else:
+         print("El nÚmero es primo")
+
+
+def main():
+    primo()
+
+if __name__=="__main__":
+    main()
